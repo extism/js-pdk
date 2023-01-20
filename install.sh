00171ba12b3bd5f1077f6d172f94248070593f71
Benjamin Eckel <bhelx@simst.im>
2023-01-20 13:58:54 -0600

    install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+OS=''
+case "$OSTYPE" in
+  darwin*)  OS="macos" ;; 
+  linux*)   OS="linux" ;;
+  *)        echo "unknown os: $OSTYPE" && exit 1 ;;
+esac
+
+ARCH=`uname -m`
+case "$ARCH" in
+  ix86*)    ARCH="x86_64" ;; 
+  arm64*)   ARCH="aarch64" ;;
+  *)        echo "unknown arch: $ARCH" && exit 1 ;;
+esac
+
+
+export TAG=v0.3.0
+curl -O "https://github.com/extism/js-pdk/releases/download/$TAG/extism-js-$ARCH-$OS-$TAG.gz"
+gunzip extism-js*.gz
+sudo mv extism-js-* /usr/local/bin/extism-js
+chmod +x /usr/local/bin/extism-js
