f97cee52ade98bd467c89b54c7e2c426c63dbf0b
Benjamin Eckel <bhelx@simst.im>
2023-11-16 12:08:39 -0600

    chore: Update the quickjs crate
    Updates the quickjs crate in preparation for some refactors and further
    work towards 1.0

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,17 +1,17 @@
 use extism_pdk::extism::error_set;
 use extism_pdk::*;
 use once_cell::sync::OnceCell;
-use quickjs_wasm_rs::Context;
+use quickjs_wasm_rs::JSContextRef;
 use std::io;
 use std::io::Read;
 
 mod globals;
 
-static mut CONTEXT: OnceCell<Context> = OnceCell::new();
+static mut CONTEXT: OnceCell<JSContextRef> = OnceCell::new();
 
 #[export_name = "wizer.initialize"]
 pub extern "C" fn init() {
-    let context = Context::default();
+    let context = JSContextRef::default();
     globals::inject_globals(&context).expect("Failed to initialize globals");
 
     let mut code = String::new();
@@ -40,7 +40,7 @@ pub unsafe extern "C" fn __invoke(func_idx: i32) -> i32 {
     0
 }
 
-fn export_names(context: &Context) -> anyhow::Result<Vec<String>> {
+fn export_names(context: &JSContextRef) -> anyhow::Result<Vec<String>> {
     let global = context.global_object().unwrap();
     let module = global.get_property("module")?;
     let exports = module.get_property("exports")?;
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const VOWELS = [
-    'a', 'e', 'i', 'o', 'u',
+  'a', 'e', 'i', 'o', 'u',
 ]
 
 function privateFunction() {
@@ -7,32 +7,32 @@ function privateFunction() {
 }
 
 export function count_vowels() {
-    let input = Host.inputString()
-    let count = privateFunction()
-    for (let i = 0; i < input.length; i++) {
-        if (VOWELS.includes(input[i].toLowerCase())) {
-            count += 1
-        }
+  let input = Host.inputString()
+  let count = privateFunction()
+  for (let i = 0; i < input.length; i++) {
+    if (VOWELS.includes(input[i].toLowerCase())) {
+      count += 1
     }
-    Host.outputString(JSON.stringify({count}))
-    return 0
+  }
+  Host.outputString(JSON.stringify({ count }))
+  return 0
 }
 
 export function greet() {
-    Host.outputString("Hello World from greet! " + Host.inputString())
-    return 0
+  Host.outputString("Hello World from greet! " + Host.inputString())
+  return 0
 }
 
 export function greet2() {
-    console.log("console log")
-    console.error("console error")
-    Var.set("thing", "variable value")
-    Host.outputBytes(Var.get("thing"))
-    return 0
+  console.log("console log")
+  console.error("console error")
+  Var.set("thing", "variable value")
+  Host.outputBytes(Var.get("thing"))
+  return 0
 }
 
 export function i_error_out() {
-    throw Error("I am an error")
+  throw Error("I am an error")
 }
 
 export function call_http() {
