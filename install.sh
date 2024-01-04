08e08dd175a09b2a8f2521c90b2cb9bb41e512c0
Benjamin Eckel <bhelx@simst.im>
2024-01-04 17:00:21 -0600

    Bump install script to rc4

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ case "$ARCH" in
 esac
 
 
-export TAG="v1.0.0-rc3"
+export TAG="v1.0.0-rc4"
 curl -L -O "https://github.com/extism/js-pdk/releases/download/$TAG/extism-js-$ARCH-$OS-$TAG.gz"
 gunzip extism-js*.gz
 sudo mv extism-js-* /usr/local/bin/extism-js
