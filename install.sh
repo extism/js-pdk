6c807a1a3c285e7a998a251b85ad6508909b1c39
jay <38236622+JayJamieson@users.noreply.github.com>
2024-01-12 12:45:09 +1300

    add download and install of binaryen bin files to install script

--- install.sh
+++ install.sh
@@ -2,21 +2,35 @@ set -e
 
 OS=''
 case `uname` in
-  Darwin*)  OS="macos" ;; 
+  Darwin*)  OS="macos" ;;
   Linux*)   OS="linux" ;;
   *)        echo "unknown os: $OSTYPE" && exit 1 ;;
 esac
 
 ARCH=`uname -m`
 case "$ARCH" in
-  ix86*|x86_64*)    ARCH="x86_64" ;; 
+  ix86*|x86_64*)    ARCH="x86_64" ;;
   arm64*|aarch64*)  ARCH="aarch64" ;;
   *)                echo "unknown arch: $ARCH" && exit 1 ;;
 esac
 
 
 export TAG="v1.0.0-rc4"
+export BINARYEN_TAG="version_116"
+
+
 curl -L -O "https://github.com/extism/js-pdk/releases/download/$TAG/extism-js-$ARCH-$OS-$TAG.gz"
+curl -L -O "https://github.com/WebAssembly/binaryen/releases/download/$BINARYEN_TAG/binaryen-$BINARYEN_TAG-$ARCH-$OS.tar.gz"
+
 gunzip extism-js*.gz
 sudo mv extism-js-* /usr/local/bin/extism-js
 chmod +x /usr/local/bin/extism-js
+
+tar xvf "binaryen-$BINARYEN_TAG-$ARCH-$OS.tar.gz"
+mv "binaryen-$BINARYEN_TAG"/ binaryen/
+sudo mkdir /usr/local/binaryen
+sudo mv binaryen/bin/wasm* /usr/local/binaryen
+
+for file in $(ls /usr/local/binaryen); do
+  sudo ln -s  /usr/local/binaryen/$file /usr/local/bin/$file
+done
