1c133ff482eb6be2a68fef57c85ad04bc64628e4
zach <zach@dylibso.com>
2024-02-13 15:20:01 -0800

    cleanup: remove imports from exports example

--- examples/exports/script.d.ts
+++ examples/exports/script.d.ts
@@ -1,10 +1,5 @@
 declare module 'main' {
   export function add3(a: I32, b: I32, c: I32): I32;
-  export function appendString(a: I64, b: I64): string;
+  export function appendString(a: string, b: string): string;
 }
 
-declare module 'extism:host' {
-  interface user {
-    testing123(a: string): {a: string};
-  }
-}
