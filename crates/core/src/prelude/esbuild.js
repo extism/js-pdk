34f264dcdc00f3b13bd1977e0b9745df3a728629
Casper Lindschouw <casper@lindschouw.me>
2024-05-09 21:38:09 +0200

    fix: change prelude format to iife, to avoid cluttering globalThis
    There's no reason this should be cjs, as it has no exports and the
    native code never calls it directly.
    
    On the contrary, minification of both the prelude and plugin code is
    likely to cause name collisions, which are painful to debug.

--- crates/core/src/prelude/esbuild.js
+++ crates/core/src/prelude/esbuild.js
@@ -7,6 +7,6 @@ esbuild
     bundle: true,
     sourcemap: true,
     minify: true,
-    format: "cjs", // needs to be CJS for now
+    format: "iife", 
     target: ["es2020"], // don't go over es2020 because quickjs doesn't support it
   });
