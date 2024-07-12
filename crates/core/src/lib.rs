d413206c282114d437ed94950740e845941e794f
Darrik Moberg <30670444+mdarrik@users.noreply.github.com>
2024-07-12 00:40:11 -0700

    feat: get lib.rs moved to rquickjs

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,13 +1,17 @@
+use anyhow::anyhow;
 use once_cell::sync::OnceCell;
 use quickjs_wasm_rs::{JSContextRef, JSValue, JSValueRef};
-use rquickjs::{Context, Runtime};
+use rquickjs::function::Args;
+use rquickjs::object::ObjectKeysIter;
+use rquickjs::{qjs::JSValue as RawValue, Context, Runtime, Value};
+use rquickjs::{Ctx, FromJs, Object};
 use std::io;
 use std::io::Read;
 
 mod globals;
 
 static mut CONTEXT: OnceCell<rquickjs::Context> = OnceCell::new();
-static mut CALL_ARGS: Vec<Vec<rquickjs::Value>> = vec![];
+static mut CALL_ARGS: Vec<Vec<ArgType>> = vec![];
 
 #[export_name = "wizer.initialize"]
 extern "C" fn init() {
@@ -21,15 +25,17 @@ extern "C" fn init() {
     let _ = context.with(|this| {
         this.eval(code)?;
         Ok::<_, rquickjs::Error>(rquickjs::Undefined)
-        
     });
 
     unsafe {
-        CONTEXT.set(context).map_err(|_| anyhow::anyhow!("Could not intialize JS Context")).unwrap()
+        CONTEXT
+            .set(context)
+            .map_err(|_| anyhow::anyhow!("Could not intialize JS Context"))
+            .unwrap()
     }
 }
 
-fn js_context<'a>() -> &'a rquickjs::Context {
+fn js_context() -> &'static rquickjs::Context {
     unsafe {
         if CONTEXT.get().is_none() {
             init()
@@ -40,72 +46,74 @@ fn js_context<'a>() -> &'a rquickjs::Context {
     }
 }
 
-fn convert_js_value<'a>(context: &'a JSContextRef, v: &JSValue) -> JSValueRef<'a> {
-    match v {
-        JSValue::Undefined => context.undefined_value().unwrap(),
-        JSValue::Null => context.null_value().unwrap(),
-        JSValue::Bool(b) => context.value_from_bool(*b).unwrap(),
-        JSValue::Int(i) => context.value_from_i32(*i).unwrap(),
-        JSValue::Float(f) => context.value_from_f64(*f).unwrap(),
-        JSValue::String(s) => context.value_from_str(s.as_str()).unwrap(),
-        JSValue::Array(a) => {
-            let arr = context.array_value().unwrap();
-            for x in a.iter() {
-                arr.append_property(convert_js_value(context, x)).unwrap();
-            }
-            arr
-        }
-        JSValue::ArrayBuffer(buf) => context.array_buffer_value(buf.as_slice()).unwrap(),
-        JSValue::Object(x) => {
-            let obj = context.object_value().unwrap();
-            for (k, v) in x.iter() {
-                obj.set_property(k.as_str(), convert_js_value(context, v))
-                    .unwrap();
-            }
-            obj
-        }
-    }
+fn convert_js_value<'js>(ctx: rquickjs::Ctx<'js>, arg: &ArgType) -> Value<'js> {
+    let v = match arg {
+        ArgType::I32(v) => rquickjs::Value::new_int(ctx, *v),
+        ArgType::I64(v) => rquickjs::BigInt::from_i64(ctx, *v).unwrap().into(),
+        ArgType::F32(v) => rquickjs::Value::new_float(ctx, *v as f64),
+        ArgType::F64(v) => rquickjs::Value::new_float(ctx, *v),
+    };
+
+    v
 }
 
-fn invoke<'a, T, F: Fn(&'a JSContextRef, JSValueRef<'a>) -> T>(
+fn invoke<'a, T, F: for<'b> Fn(Ctx<'b>, rquickjs::Value<'b>) -> T>(
     idx: i32,
     conv: F,
 ) -> Result<T, anyhow::Error> {
     let call_args = unsafe { CALL_ARGS.pop() };
     let context = js_context();
-    let args: Vec<_> = call_args
-        .unwrap()
-        .iter()
-        .map(|x| convert_js_value(context, x))
-        .collect();
+    let result = context.with(|ctx| {
+        let call_args = call_args.unwrap();
+        let args: Args = call_args.iter().fold(
+            Args::new(ctx.clone(), call_args.len()),
+            |mut args, rust_arg| {
+                match rust_arg {
+                    ArgType::I32(v) => args
+                        .push_arg(v)
+                        .expect("Should be able to convert i32 to JS arg"),
+                    ArgType::I64(v) => args
+                        .push_arg(v)
+                        .expect("Should be able to convert i64 to JS arg"),
+                    ArgType::F32(v) => args
+                        .push_arg(v)
+                        .expect("Should be able to convert f32 to JS arg"),
+                    ArgType::F64(v) => args
+                        .push_arg(v)
+                        .expect("Should be able to convert f64 to JS arg"),
+                }
+                args
+            },
+        );
 
-    let global = context.global_object().unwrap();
+        let global = ctx.globals();
 
-    let module = global.get_property("module")?;
-    let exports = module.get_property("exports")?;
+        let module: Object = global.get("module")?;
+        let exports: Object = module.get("exports")?;
 
-    let export_names = export_names(exports).unwrap();
+        let export_names = export_names(exports.clone()).unwrap();
 
-    let function = exports
-        .get_property(export_names[idx as usize].as_str())
-        .unwrap();
-    let function_invocation_result = function.call(&context.undefined_value().unwrap(), &args);
+        let function: rquickjs::Function =
+            exports.get(export_names[idx as usize].as_str()).unwrap();
 
-    while context.is_pending() {
-        context.execute_pending()?;
-    }
+        let function_invocation_result = function.call_arg(args);
 
-    match function_invocation_result {
-        Ok(r) => Ok(conv(context, r)),
-        Err(err) => {
-            let e = format!("{:?}", err);
-            let mem = extism_pdk::Memory::from_bytes(&e).unwrap();
-            unsafe {
-                extism_pdk::extism::error_set(mem.offset());
+        match function_invocation_result {
+            Ok(r) => {
+                let res = conv(ctx.clone(), r);
+                Ok(res)
             }
-            Err(err)
+            Err(err) => {
+                let e = format!("{:?}", err);
+                let mem = extism_pdk::Memory::from_bytes(&e).unwrap();
+                unsafe {
+                    extism_pdk::extism::error_set(mem.offset());
+                }
+                Err(err)
+            }
         }
-    }
+    })?;
+    Ok(result)
 }
 
 #[no_mangle]
@@ -118,34 +126,28 @@ pub extern "C" fn __arg_start() {
 #[no_mangle]
 pub extern "C" fn __arg_i32(arg: i32) {
     unsafe {
-        CALL_ARGS.last_mut().unwrap().push(JSValue::Int(arg));
+        CALL_ARGS.last_mut().unwrap().push(ArgType::I32(arg));
     }
 }
 
 #[no_mangle]
 pub extern "C" fn __arg_i64(arg: i64) {
     unsafe {
-        CALL_ARGS
-            .last_mut()
-            .unwrap()
-            .push(JSValue::Float(arg as f64));
+        CALL_ARGS.last_mut().unwrap().push(ArgType::I64(arg));
     }
 }
 
 #[no_mangle]
 pub extern "C" fn __arg_f32(arg: f32) {
     unsafe {
-        CALL_ARGS
-            .last_mut()
-            .unwrap()
-            .push(JSValue::Float(arg as f64));
+        CALL_ARGS.last_mut().unwrap().push(ArgType::F32(arg));
     }
 }
 
 #[no_mangle]
 pub extern "C" fn __arg_f64(arg: f64) {
     unsafe {
-        CALL_ARGS.last_mut().unwrap().push(JSValue::Float(arg));
+        CALL_ARGS.last_mut().unwrap().push(ArgType::F64(arg));
     }
 }
 
@@ -167,22 +169,28 @@ macro_rules! unwrap_value {
 
 #[no_mangle]
 pub extern "C" fn __invoke_i32(idx: i32) -> i32 {
-    unwrap_value!(-1, invoke(idx, |_ctx, r| r.as_i32_unchecked()))
+    unwrap_value!(-1, invoke(idx, |_ctx, r| r.as_number().unwrap() as i32))
 }
 
 #[no_mangle]
 pub extern "C" fn __invoke_i64(idx: i32) -> i64 {
-    unwrap_value!(-1, invoke(idx, |_ctx, r| r.as_f64_unchecked() as i64))
+    unwrap_value!(
+        -1,
+        invoke(idx, |_ctx, r| (r.as_big_int().unwrap())
+            .clone()
+            .to_i64()
+            .unwrap())
+    )
 }
 
 #[no_mangle]
 pub extern "C" fn __invoke_f64(idx: i32) -> f64 {
-    unwrap_value!(-1.0, invoke(idx, |_ctx, r| r.as_f64_unchecked()))
+    unwrap_value!(-1.0, invoke(idx, |_ctx, r| r.as_float().unwrap()))
 }
 
 #[no_mangle]
 pub extern "C" fn __invoke_f32(idx: i32) -> f32 {
-    unwrap_value!(-1.0, invoke(idx, |_ctx, r| r.as_f64_unchecked() as f32))
+    unwrap_value!(-1.0, invoke(idx, |_ctx, r| r.as_number().unwrap() as f32))
 }
 
 #[no_mangle]
@@ -190,14 +198,21 @@ pub extern "C" fn __invoke(idx: i32) {
     unwrap_value!((), invoke(idx, |_ctx, _r| ()))
 }
 
-fn export_names(exports: JSValueRef<'static>) -> anyhow::Result<Vec<String>> {
-    let mut properties = exports.properties()?;
-    let mut key = properties.next_key()?;
+fn export_names(exports: rquickjs::Object) -> anyhow::Result<Vec<String>> {
+    let mut keys_iter: ObjectKeysIter<String> = exports.keys();
+    let mut key = keys_iter.next();
     let mut keys: Vec<String> = vec![];
     while key.is_some() {
-        keys.push(key.unwrap().as_str()?.to_string());
-        key = properties.next_key()?;
+        keys.push(key.unwrap()?.to_string());
+        key = keys_iter.next();
     }
     keys.sort();
     Ok(keys)
+}
+
+enum ArgType {
+    I32(i32),
+    I64(i64),
+    F32(f32),
+    F64(f64),
 }
