ad9eec3b433bd1d817d40f58af42b93bee8cd6fe
Muhammad Azeez <muhammad@dylibso.com>
2025-01-16 17:36:04 +0300

    fix to_base64

--- examples/base64/script.js
+++ examples/base64/script.js
@@ -1,18 +1,22 @@
 function greet() {
-  const base64String = "SGVsbG8g8J+MjSBXb3JsZCHwn4yN";
+  try {
+    const base64String = "SGVsbG8g8J+MjSBXb3JsZCHwn4yN";
 
-  console.log('decoding string:', base64String);
+    console.log('decoding string:', base64String);
 
-  const decodedBuffer = Host.base64ToArrayBuffer(base64String);
-  const decodedString = new TextDecoder().decode(decodedBuffer);
+    const decodedBuffer = Host.base64ToArrayBuffer(base64String);
+    const decodedString = new TextDecoder().decode(decodedBuffer);
 
-  console.log('decoded string:', decodedString);
+    console.log('decoded string:', decodedString);
 
-  const encodedBuffer = Host.arrayBufferToBase64(decodedBuffer);
-  
-  console.log('encoded string:', encodedBuffer);
+    const encodedBuffer = Host.arrayBufferToBase64(decodedBuffer);
 
-  Host.outputString(`Hello, ${Host.inputString()}`)
+    console.log('encoded string:', encodedBuffer);
+
+    Host.outputString(`Hello, ${Host.inputString()}`)
+  } catch (error) {
+    console.error('Base64 test failed, Error:', JSON.stringify(error));
+  }
 }
 
 module.exports = { greet };
