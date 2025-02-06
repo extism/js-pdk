db850c09724b762a726ff5874e37f21166aee476
Muhammad Azeez <muhammad@dylibso.com>
2025-02-06 12:21:40 +0300

    feat: support i32, i64, f32, f64 param/return types for Host functions

--- examples/host_funcs/script.d.ts
+++ examples/host_funcs/script.d.ts
@@ -4,7 +4,9 @@ declare module 'main' {
 
 declare module 'extism:host' {
   interface user {
-    myHostFunction1(ptr: PTR): PTR;
-    myHostFunction2(ptr: PTR): PTR;
+    capitalize(ptr: PTR): PTR;
+    floatInputs(p1: F64, p2: F32): I32;
+    floatOutput(p1: I32): F64;
+    voidInputs(p1: I32, p2: I64, p3: F32, p4: F64, p5: I32): void;
   }
 }
